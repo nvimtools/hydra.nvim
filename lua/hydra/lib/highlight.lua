ababa9d412a20d9a6479167fe15508b8099fc91d
Frederick Zhang <frederick888@tsundere.moe>
2024-01-02 03:27:49 +1100

    fix: Avoid grabbing invalid highlight keys (#18)
    Co-authored-by: benlubas <benmlubas@gmail.com>

--- lua/hydra/lib/highlight.lua
+++ lua/hydra/lib/highlight.lua
@@ -1,16 +1,10 @@
 local api = vim.api
 
-local function get_hl(name)
-   ---@type boolean
-   local rgb = api.nvim_get_option('termguicolors')
-   return api.nvim_get_hl_by_name(name, rgb)
-end
-
 local name, settings
 for _, color in ipairs({ 'Red', 'Blue', 'Amaranth', 'Teal', 'Pink' }) do
    settings = vim.tbl_deep_extend('force',
-      get_hl('StatusLine'),
-      get_hl(string.format('Hydra%s', color))
+      api.nvim_get_hl(0, { name = 'StatusLine', link = false }),
+      api.nvim_get_hl(0, { name = string.format('Hydra%s', color), link = false })
    )
    name = string.format('HydraStatusLine%s', color)
    api.nvim_set_hl(0, name, settings)
